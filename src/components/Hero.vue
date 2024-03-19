a141b9cd9361a9741b194ee8fea3c3decf499394
Michał Korotczuk <michal.korotczuk@idweaver.com>
2024-03-19 20:17:36 +0100

    hero button fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,14 +3,12 @@
     <div class="mx-5 md-mx-l5">
       <div>
         <h1 class="teal opacity-80 fs-l3 lh-2 md-fs-xl1 md-lh-1 fw-900">Elevate Your <br />Events Now</h1>
-        <div class="br-8 mt-10 inline-flex">
+        <div class="br-8 mt-10 inline-flex backdrop-blur-10">
           <input
             type="text"
-            class="input-lg half bw-0 fw-200 bg-indigo-lightest-10 backdrop-blur-10 white ph-white focus-white opacity-80 fs-s3 py-5 min-w-25vw br-r-0"
+            class="input-lg half bw-0 fw-200 bg-transparent white ph-white focus-white opacity-80 fs-s3 py-5 min-w-25vw br-r-0"
             placeholder="Email Address" />
-          <button class="button-lg bg-indigo-lightest-10 backdrop-blur-10 white focus-white fw-300 fs-s3 mr-0 br-l-0">
-            Get Started
-          </button>
+          <button class="hero-button button-lg bg-transparent white focus-white fw-300 fs-s3 mr-0 br-l-0">Get Started</button>
         </div>
         <div class="white opacity-40 fs-s3 mt-3">No credit card required</div>
       </div>
@@ -33,6 +31,10 @@
 
   @include large {
     background-image: url("../assets/hero-img-lg.jpg");
+  }
+
+  .hero-button {
+    border-left: 1px solid rgba(255, 255, 255, 0.25);
   }
 }
 </style>
